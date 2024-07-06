1818fb6f6cfca3900c43b0150ed9e54d69cd470c
Shahreyar Arif <70688937+Shahreyar00@users.noreply.github.com>
2024-07-06 12:20:08 +0530

    Routes for auth and user functionalities coupled with client implementation of register and login feature

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./navbar.scss";
+
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const user = false;
+
+    return (
+        <nav>
+            <div className="left">
+                <a href="/" className="logo">
+                    <img src="/logo.png" alt="" />
+                    <span>NomadEstate</span>
+                </a>
+                <a href="/">Home</a>
+                <a href="/list">Properties</a>
+                <a href="/">Contact</a>
+                <a href="/">Agents</a>
+            </div>
+            <div className="right">
+                {user ? (
+                    <div className="user">
+                        <img
+                            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                            alt=""
+                        />
+                        <span>John Doe</span>
+                        <Link to="/profile" className="profile">
+                            <div className="notification">3</div>
+                            <span>Profile</span>
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        <a href="/login" className="login">Sign in</a>
+                        <a href="/register" className="register">
+                            Sign up
+                        </a>
+                    </>
+                )}
+                <div className="menuIcon">
+                    <img
+                        src="/menu.png"
+                        alt=""
+                        onClick={() => setOpen((prev) => !prev)}
+                    />
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                    <a href="/">Home</a>
+                    <a href="/list">Properties</a>
+                    <a href="/">Contact</a>
+                    <a href="/">Agents</a>
+                    <a href="/login">Sign in</a>
+                    <a href="/register">Sign up</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
